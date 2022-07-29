2708c2f39ce5db48b7fe80d8c1ba925078fe5415
Tanner Richards <Tanner.richards990@gmail.com>
2022-07-29 13:05:27 -0700

    forgot to commit early, but have succesfully setup the first two arrays, renders, and functions. CSS is still causing some issues, but will continue to play for desired results

--- app.js
+++ app.js
@@ -1,4 +1,29 @@
 // import functions and grab DOM elements
+import { cars } from './array-cars.js';
+import { renderCar } from './render-car.js';
+
+import { renderBrand } from './car-two-render.js';
+import { carsTwo } from './array-cars-two.js';
+
+const carListEl = document.getElementById('car-list');
+const carTwoEl = document.getElementById('car-list-two');
+
+
+for (let car of cars) {
+    const singleCar = renderCar(car);
+    carListEl.append(singleCar);
+}
+
+
+for (let brand of carsTwo) {
+    const carbrandEl = renderBrand(brand);
+    carTwoEl.append(carbrandEl);
+}
+
+
+
+
+
 
 // initialize global state
 
